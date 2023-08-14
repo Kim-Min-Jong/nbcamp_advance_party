ebbbd3b866a83e0d5f5c29884db52dd9424713a2
minjong kim <alswhddkdl@gmail.com>
2023-08-14 12:59:14 +0900

    feat: todo - bookmarked 이동 시 todo에만 fab가 보이게 설정 및 fab 버튼 클릭 시 todo 작성 페이지로 넘어가는 기능 구현

--- week1/app/src/main/java/com/sparta/week1/MainActivity.kt
+++ week1/app/src/main/java/com/sparta/week1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.sparta.week1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sparta.week1.adapter.FragmentAdapter
 import com.sparta.week1.databinding.ActivityMainBinding
@@ -21,7 +23,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViews()
+        initButton()
     }
+
 
     private fun initViews() = with(binding) {
         pager.adapter = FragmentAdapter(this@MainActivity)
@@ -31,5 +35,20 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tabLayout, pager) { tab, pos ->
             tab.text = titleList[pos]
         }.attach()
+        pager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when(position){
+                    0 -> fabAddWriting.show()
+                    else -> fabAddWriting.hide()
+                }
+            }
+        })
+
+    }
+
+    private fun initButton() = with(binding) {
+        fabAddWriting.setOnClickListener {
+            startActivity(Intent(this@MainActivity, TodoWritingActivity::class.java))
+        }
     }
 }
